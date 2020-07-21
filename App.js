083fddfeb41c13f4e54176b8b0710dd5faeb1d5d
Alan Churichi <achurichi@gmail.com>
2020-07-21 16:49:56 -0300

    WebView configuration

--- App.js
+++ App.js
@@ -1,21 +1,20 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react'
+import { StyleSheet } from 'react-native'
+import { WebView } from 'react-native-webview'
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+	return (
+		<WebView
+			source={{ uri: 'http://192.168.0.125:8000/index.html' }}
+			style={styles.container}
+		/>
+	)
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+	container: {
+		flex: 1,
+		height: '100%',
+		width: '100%',
+	},
+})
